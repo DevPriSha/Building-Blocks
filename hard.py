322a7554fd6d03ad744d578570569ff786b679fa
chandana-21 <chandanakuntala21@gmail.com>
2021-09-30 22:59:51 +0530

    added hard questions

---
+++ hard.py
@@ -0,0 +1,18 @@
+hard = {
+    'Regular Expression - Check if divisible by 0b111 (7)':'https://www.codewars.com/kata/56a73d2194505c29f600002d/train/',
+    'Blaine is a pain':'https://www.codewars.com/kata/59b47ff18bcb77a4d1000076/train/',
+    'Insane Coloured Triangles':'https://www.codewars.com/kata/5a331ea7ee1aae8f24000175/train/',
+    'Symbolic differentiation of prefix expressions':'https://www.codewars.com/kata/584daf7215ac503d5a0001ae/train/',
+    'Expression Transpiler':'https://www.codewars.com/kata/597ccf7613d879c4cb00000f/train/',
+    '6 By 6 Skyscrapers':'https://www.codewars.com/kata/5679d5a3f2272011d700000d/train/',
+    'Gerrymander Solver':'https://www.codewars.com/kata/5a70285ab17101627a000024/train/',
+    'Cut the cake':'https://www.codewars.com/kata/586214e1ef065414220000a8/train/',
+    'Break the pieces':'https://www.codewars.com/kata/527fde8d24b9309d9b000c4e/train/',
+    '7×7 Skyscrapers':'https://www.codewars.com/kata/5917a2205ffc30ec3a0000a8/train/',
+    'BECOME IMMORTAL':'https://www.codewars.com/kata/59568be9cc15b57637000054/train/',
+    'Square sums':'https://www.codewars.com/kata/5a667236145c462103000091/train/',
+    'Sliding Puzzle Solver':'https://www.codewars.com/kata/5a20eeccee1aae3cbc000090/train/',
+    'The Lift':'https://www.codewars.com/kata/58905bfa1decb981da00009e/train/',
+    'Centre of attention':'https://www.codewars.com/kata/58c8c723df10450b21000024/train/',
+    'The boolean order':'https://www.codewars.com/kata/59eb1e4a0863c7ff7e000008/train/'
+}
